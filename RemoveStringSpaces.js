9d136cd649a5f989b291f42865a2ce0373faf891
Cem <cmbicakci@gmail.com>
2022-12-12 23:25:41 +0300

    revised

--- RemoveStringSpaces.js
+++ RemoveStringSpaces.js
@@ -1,16 +1,7 @@
 //Simple, remove the spaces from the string, then return the resultant string.
 
 function noSpace(x) {
-    const arr = x.split('');
-
-    const newArr = []
-    for (let i = 0; i < arr.length; i++) {
-        
-        const trim = arr[i].trim();
-        newArr.push(trim);
-    }
-
-    return newArr.join('')
+    return x.split(' ').join('')
 }
 
 console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
